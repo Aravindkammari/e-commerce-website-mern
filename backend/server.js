9a508d747900c1e8cd77af10a34674e92afb1800
Aravindkammari <aravind.kammari07@gmail.com>
2025-05-16 23:09:25 +0530

    server created

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+})
+.then(()=>{
+    console.log('mongodb connected successfully');
+})
+.catch((err)=>console.error(err));
+
+const port = process.eventNames.PORT || 4000;
+app.listen(port, ()=>{
+    console.log(`server running successfully on port ${port}`);
+})
+
